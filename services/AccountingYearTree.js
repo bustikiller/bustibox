747c69244263191125224cf7e51f1fa242aa5f51
Manuel Bustillo <mayn13@gmail.com>
2018-05-20 18:10:14 +0200

    Refactor accounting year tree building

--- services/AccountingYearTree.js
+++ services/AccountingYearTree.js
@@ -11,36 +11,41 @@ export default class AccountingYearTree {
 	build(callback) {
 		console.log("Building AccountingYearTree for year " + this.year);
 		new Database().fetchNodes("partida_contable", nodes => {
-			nodes.forEach(node => {
-				this.entriesMap[node.nid] = {node: node, children: [], leaves: []};
-				console.log("Loaded accounting entry " + node.nid);
-			});
-
-			console.log("Finished loading accounting entries into memory");
-
-			nodes.forEach(node => {
-				parentNid = node.partida_padre;
-				if(parentNid){
-					if(!this.entriesMap[parentNid]){
-						console.log("ERROR: Parent node with id "+ parentNid + " was not found");
-					} else {
-						this.entriesMap[parentNid].children.push(this.entriesMap[node.nid])
-					}
-				}
-			});
-
-			let filteredEntries = Object.values(this.entriesMap).filter(entry => {
-				return (parseInt(entry.node.anio_contable) === this.year);
-			})
-
-			this.entries = filteredEntries;
-
+			this.initAccountingEntriesMap(nodes);
+			this.loadAccountingEntries(nodes);
 			this.loadAccountingSeats(callback);
 		});
 	}
 
-	loadAccountingSeats(callback){
+	initAccountingEntriesMap(nodes){
+		nodes.forEach(node => {
+			this.entriesMap[node.nid] = {node: node, children: [], leaves: []};
+			console.log("Loaded accounting entry " + node.nid);
+		});
 
+		console.log("Finished loading accounting entries into memory");
+	}
+
+	loadAccountingEntries(nodes){
+		nodes.forEach(node => {
+			parentNid = node.partida_padre;
+			if(parentNid){
+				if(!this.entriesMap[parentNid]){
+					console.log("ERROR: Parent node with id "+ parentNid + " was not found");
+				} else {
+					this.entriesMap[parentNid].children.push(this.entriesMap[node.nid])
+				}
+			}
+		});
+
+		let filteredEntries = Object.values(this.entriesMap).filter(entry => {
+			return (parseInt(entry.node.anio_contable) === this.year);
+		})
+
+		this.entries = filteredEntries;
+	}
+
+	loadAccountingSeats(callback){
 		new Database().fetchNodes("asiento_contable", nodes => {
 			nodes.forEach(node => {
 				parentNid = node.partida_contable;
@@ -55,6 +60,5 @@ export default class AccountingYearTree {
 
 			callback();
 		});
-		
 	}
 }
